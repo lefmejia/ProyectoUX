735784d6ca4c90d5c0aa02ed47fc54e5b6613f8d
LEONEL ALBERTO THOMAS VILLALOBOS <leothomasv@unitec.edu>
2021-09-04 15:07:58 -0600

    Mejoras en el login y crear cuenta

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Router } from '@angular/router';
-import { Auth, GoogleAuthProvider } from 'firebase/auth'
+import { Auth, GoogleAuthProvider } from 'firebase/auth';
 import Swal from 'sweetalert2';
 
 @Component({
@@ -12,68 +12,79 @@ import Swal from 'sweetalert2';
 export class FormComponent implements OnInit {
   @Input() action: string;
 
-  email = 'leothomasv@example.com';
-  pass = '123456';
+  email = '';
+  pass = '';
+  nombre = '';
+  apellido = '';
+  direccion = '';
 
-  constructor(
-    public auth: AngularFireAuth,
-    private router: Router ) {}
+  constructor(public auth: AngularFireAuth, private router: Router) {}
 
   ngOnInit(): void {
     console.log(this.action);
   }
 
-  loginWithGoogle(){
-    this.auth.signInWithPopup(new GoogleAuthProvider).then(res =>{
+  loginWithGoogle() {
+    this.auth.signInWithPopup(new GoogleAuthProvider()).then((res) => {
       this.router.navigate(['welcome']);
       Swal.fire({
         position: 'top-end',
         icon: 'success',
         title: 'LogIn Succesful',
         showConfirmButton: false,
-        timer: 1500
-      })
-    })
+        timer: 1500,
+      });
+    });
   }
-  
-  logOut(){
-    this.auth.signOut()
+
+  logOut() {
+    this.auth.signOut();
     Swal.fire({
       position: 'top-end',
       icon: 'warning',
       title: 'LogOut Succesful',
       showConfirmButton: false,
-      timer: 1500
-    })
+      timer: 1500,
+    });
   }
 
-  customLogin(){
-    this.auth.signInWithEmailAndPassword(this.email, this.pass).then(res =>{
-      console.log(res);
-      this.router.navigate(['welcome']);
-      Swal.fire({
-        position: 'top-end',
-        icon: 'success',
-        title: 'LogIn Succesful',
-        showConfirmButton: false,
-        timer: 1500
+  customLogin() {
+    this.auth
+      .signInWithEmailAndPassword(this.email, this.pass)
+      .then((res) => {
+        console.log(res);
+        this.router.navigate(['welcome']);
+        Swal.fire({
+          position: 'top-end',
+          icon: 'success',
+          title: 'LogIn Succesful',
+          showConfirmButton: false,
+          timer: 1500,
+        });
       })
-    })
-    .catch(err => console.log('Error', err));
+      .catch((err) => Swal.fire({
+        position: 'top-end',
+        icon: 'warning',
+        title: 'Usuario no encontrado',
+        showConfirmButton: false,
+        timer: 1500,
+      }));
   }
 
-  register(){
-    this.auth.createUserWithEmailAndPassword(this.email, this.pass).then(user =>{
-      console.log(user);
-      this.router.navigate(['welcome']);
-      Swal.fire({
-        position: 'top-end',
-        icon: 'success',
-        title: 'Cuenta creada exitosamente',
-        showConfirmButton: false,
-        timer: 1500
+  register() {
+    this.auth
+      .createUserWithEmailAndPassword(this.email, this.pass)
+      .then((user) => {
+        console.log(user);
+        this.router.navigate(['login']);
+        Swal.fire({
+          position: 'top-end',
+          icon: 'success',
+          title: 'Cuenta creada exitosamente',
+          showConfirmButton: false,
+          timer: 1500,
+        });
       })
-    })
-    .catch(err => console.log('Error user: ', err));
+      .catch((err) => console.log('Error user: ', err));
   }
 }
