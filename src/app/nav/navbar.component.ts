31f0dfb54fc7d0f084fa7ff0b481749fc0bbc334
LEONEL ALBERTO THOMAS VILLALOBOS <leothomasv@unitec.edu>
2021-09-04 19:05:22 -0600

    css

--- src/app/nav/navbar.component.ts
+++ src/app/nav/navbar.component.ts
@@ -3,17 +3,6 @@ import { Component } from "@angular/core";
 @Component({
   selector: "crunchy-navbar",
   templateUrl: "./navbar.component.html",
-  styles: [
-    `
-      .navbar-default {
-        background-color: #bc93e4;
-        color: white;
-      }
-
-      a {
-        color: white;
-      }
-    `,
-  ],
+  styleUrls:['./navbar.component.css']
 })
 export class NavBarComponent {}
