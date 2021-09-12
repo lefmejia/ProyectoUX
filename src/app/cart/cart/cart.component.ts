43f51752f245fb5e012e0ca86cf0529343fd901d
Luis <lefmejia@unitec.edu>
2021-09-12 13:12:19 -0600

    Mandar email de orden

--- src/app/cart/cart/cart.component.ts
+++ src/app/cart/cart/cart.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { CartService } from 'src/app/services/cart.service';
+import { PizzaServiceService } from 'src/app/services/pizza-service.service';
+import {Email} from './../../../assets/smtp.js';
+//declare let Email: any;
 
 @Component({
   selector: 'app-cart',
@@ -11,7 +15,7 @@ export class CartComponent implements OnInit {
   public products: any = [];
   public grandTotal !: number;
 
-  constructor(private cartService: CartService) { }
+  constructor(private cartService: CartService, public service: PizzaServiceService, private router: Router) { }
 
   ngOnInit(): void {
     this.cartService.getProducts()
@@ -19,6 +23,47 @@ export class CartComponent implements OnInit {
       this.products = res;
       this.grandTotal = this.cartService.getTotalPrice();
     })
+  }
+
+  sendEmail() {
+
+    // this.service.auth.onAuthStateChanged(function(user) {
+    //   if (user) {
+    //     // User is signed in.
+    //   } else {
+    //     // No user is signed in.
+    //   }
+    // });
+    //[disabled]="this.products.length === 0"
+    this.service.auth.user.subscribe((data) => {
+      if(data == null){
+        this.router.navigate(["/login"]);
+        return;
+      }
+      else
+      {
+        let mensaje =`
+        Detalle de la orden:<br/>
+      `;
+      this.products.forEach(product => {
+        mensaje += `${product.nombre}\t\t    L. ${product.precio} <br/>`;
+      });
+  
+      Email.send({
+        Host: "smtp.gmail.com",
+        Username: "crunchyrollpizza@gmail.com",
+        Password: "Proyectoux123",
+        From: "crunchyrollpizza@gmail.com",
+        To: "crunchyrollpizza@gmail.com",
+        Subject: "NUEVA ORDEN!!!!!",
+        Body: mensaje,
+      }).then(function (message) {
+        alert("correo enviado exitosamente")
+      });
+      }
+
+    });
+    
   }
 
 }
